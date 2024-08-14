682bf99bdee7c13b1c4604c3440a571efeb2e1cf
Deveshi Singh <deveshi@Deveshis-Laptop.local>
2024-08-14 22:53:25 +0530

    Updates

---
+++ precache-manifest.c41d9248d8fde9951cf26e47efd9f6b2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaa123fbe85a8aaae9834742781ec6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fbc042c2eb85428da2d",
+    "url": "/static/css/2.b6ea3bd1.chunk.css"
+  },
+  {
+    "revision": "00d7532a00ee9a8fb201",
+    "url": "/static/css/main.856acaf8.chunk.css"
+  },
+  {
+    "revision": "8fbc042c2eb85428da2d",
+    "url": "/static/js/2.3ec20b7c.chunk.js"
+  },
+  {
+    "revision": "32f9e993af419b3f82b39fb51b22bd8b",
+    "url": "/static/js/2.3ec20b7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "00d7532a00ee9a8fb201",
+    "url": "/static/js/main.8b4b515d.chunk.js"
+  },
+  {
+    "revision": "05e2d2637e83c42dc136",
+    "url": "/static/js/runtime-main.4ee28378.js"
+  },
+  {
+    "revision": "ead682580f521084886e152d2bf6e56a",
+    "url": "/static/media/bg_1.ead68258.jpg"
+  },
+  {
+    "revision": "b74c1a06dc24f97c5c56964d31085553",
+    "url": "/static/media/bg_2.b74c1a06.jpg"
+  },
+  {
+    "revision": "c2be1f827363b93d8666f1fd92a1e2c2",
+    "url": "/static/media/bg_3.c2be1f82.jpg"
+  },
+  {
+    "revision": "b59594b23e325951362528fbe4a0acc8",
+    "url": "/static/media/category-1.b59594b2.jpg"
+  },
+  {
+    "revision": "e975c99d7863606d5342bee409a11f62",
+    "url": "/static/media/category-2.e975c99d.jpg"
+  },
+  {
+    "revision": "9a73352f7a604700dd7474a5ca01b5cb",
+    "url": "/static/media/category-3.9a73352f.jpg"
+  },
+  {
+    "revision": "600671909d55d8db086ae2962e5c75c2",
+    "url": "/static/media/category-4.60067190.jpg"
+  },
+  {
+    "revision": "22aa382b7d25e545c7292c12c41fc5bc",
+    "url": "/static/media/category.22aa382b.jpg"
+  },
+  {
+    "revision": "259097aa534fcf6084e27f3d8b7c05fb",
+    "url": "/static/media/farm.259097aa.jpg"
+  },
+  {
+    "revision": "b35cd9fc8228ad1f4b06476ee284f87c",
+    "url": "/static/media/jumbo.b35cd9fc.jpg"
+  },
+  {
+    "revision": "bbcd5413ffcc46f7d9eef5fb36340659",
+    "url": "/static/media/person_1.bbcd5413.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/static/media/person_2.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/static/media/person_3.514eee1b.jpg"
+  },
+  {
+    "revision": "cfb95125014362aa10870999ff5d645d",
+    "url": "/static/media/person_4.cfb95125.jpg"
+  }
+]);
