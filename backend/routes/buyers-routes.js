a1b65a6191daf31968fa580b83cc4308d4128302
memyselfandglitch <deveshisingh7b@gmail.com>
2024-08-18 17:10:16 +0530

    Delete backend directory

--- backend/routes/buyers-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const buyersControllers = require('../controllers/buyers-controllers');
-const router = express.Router();
-
-router.get('/', buyersControllers.getProducts);
-
-module.exports = router;
